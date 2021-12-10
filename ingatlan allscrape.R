313d7962dfbcabc29b9f25f9f11c9e03a6ca45db
kanyipi <kanyipicske@gmail.com>
2021-12-10 10:46:32 +0100

    update

--- ingatlan allscrape.R
+++ ingatlan allscrape.R
@@ -6,22 +6,28 @@ library(data.table)
 library(tidyverse)
 
 get_one_house <- function(url) {
+  
   t_list <- list()
 
   t <- read_html(url)
+  
   t_list[["address"]] <- t %>%
     html_nodes(".address") %>%
     html_text() %>%
     trimws()
+  
   t_list[["price"]] <- t %>%
     html_nodes(".parameterTitleLink+ .parameterValues span") %>%
     html_text()
+  
   t_list[["area"]] <- t %>%
     html_nodes(".parameter:nth-child(2) span") %>%
     html_text()
+  
   t_list[["noroom"]] <- t %>%
     html_nodes(".parameter~ .parameter+ .parameter .parameterValues , .restricted") %>%
     html_text()
+  
   t_list[["description"]] <- t %>%
     html_nodes(".longDescription") %>%
     html_text()
@@ -30,17 +36,24 @@ get_one_house <- function(url) {
     html_nodes(".parameterName") %>%
     html_text() %>%
     trimws()
+  
   values <- t %>%
     html_nodes(".parameterValue") %>%
     html_text() %>%
     trimws()
+  
   if (length(keys) == length(values)) {
     for (i in 1:length(keys)) {
       t_list[[keys[i]]] <- values[i]
     }
   }
+  
   return(t_list)
 }
+
+# get_one_house gets the data points for one house namely:
+# the address, the price, the size, the number of rooms,
+# the description and the list data values e.g.: the year of construction, the condition and so on
 
 # test
 # url <- 'https://ingatlan.com/xvi-ker/elado+lakas/tegla-epitesu-lakas/32395911'
@@ -48,38 +61,61 @@ get_one_house <- function(url) {
 
 get_links_on_page <- function(url) {
   t <- read_html(url)
+  
   rel_links <- t %>%
     html_nodes(".listing__link") %>%
     html_attr("href")
+  
   links <- paste0("https://ingatlan.com", rel_links)
+  
   return(links)
 }
+
+# get_links_on_page gets every relative link on a ingatlan.com search page
+# and coverst them to an absolute link
 
 # test
 # url <- 'https://ingatlan.com/lista/elado+lakas+xvi-ker?page=2'
 # get_links_on_page(url)
 
-## TODO FUNCTION AND I FOR LINKS
-get_read_property <- function() {
-  for (i in 0:34) {
+
+get_read_property <- function(nameofcsvs,linktoscrape,noofpagestoget=0,batchsize=50) {
+  
+  dir.create(nameofcsvs)
+
+  lastpagetext <- read_html(linktoscrape) %>% 
+    html_nodes(".pagination__page-number") %>%
+    html_text() %>%
+    trimws()
+  
+  lastpageno <- lastpagetext %>% 
+    substr(2,nchar(lastpagetext)) %>% 
+    parse_number()
+    
+  lastpagebatch <- floor(lastpageno/batchsize)+1
+  
+  for (i in 0:lastpagebatch) {
     print(paste("start", i))
-    si <- ((i * 50) + 1)
-    se <- (((i + 1) * 50) + 1)
-    lastpageurl <- "https://ingatlan.com/lista/elado+lakas+budapest+ar-szerint"
-    t <- read_html(lastpageurl)
-    lastpage <- t %>%
-      html_nodes(".pagination__page-number") %>%
-      html_text() %>%
-      trimws() %>%
-      substr(5, 8) %>%
-      strtoi()
-    if (lastpage < se) {
-      se <- lastpage
+    
+    si <- ((i * batchsize) + 1)
+    se <- (((i + 1) * batchsize) + 1)
+    
+    if (lastpageno < se) {
+      se <- lastpageno
     }
-    links_to_get_links <- paste0("https://ingatlan.com/lista/elado+lakas+budapest+ar-szerint?page=", si:se)
+    
+    if (noofpagestoget!=0) {
+      if (noofpagestoget < se) {
+        se <- noofpagestoget
+      }
+    }
+    
+    links_to_get_links <- paste0(linktoscrape,"?page=", si:se)
     link_list <- sapply(links_to_get_links, get_links_on_page)
+    
     data_list <- list()
     k <- 0
+    
     for (j in link_list) {
       tryCatch(
         {
@@ -97,10 +133,11 @@ get_read_property <- function() {
       # print(length(data_list))
     }
     df <- rbindlist(data_list, fill = T)
-    csvname <- paste0("budapest", i, ".csv")
+    csvname <- paste0(nameofcsvs,"/",nameofcsvs, i, ".csv")
     write.csv(df, csvname)
   }
 }
+
 
 # old read
 # for (i in 1:34) {
@@ -112,11 +149,27 @@ get_read_property <- function() {
 #  write.csv(df, csvname)
 # }
 
-get_all_property <- function() {
-  houses <- read.csv("budapest0.csv")
+
+
+
+get_all_property <- function(nameofcsv) {
+  
+  csv_list <- list.files(nameofcsv)
+  max_file_no <- 0
+  
+  for (i in csv_list) {
+    fileno <- parse_number(i)
+    if (fileno>max_file_no) {
+      max_file_no <- fileno
+    }
+  }
+  
+  houses <- read.csv(nameofcsv,"0.csv")
+  
   df <- subset(houses, select = -Panelprogram)
-  for (i in 1:34) {
-    houses <- read.csv(paste0("budapest", i, ".csv"))
+  
+  for (i in 1:max_file_no) {
+    houses <- read.csv(paste0(nameofcsv, i, ".csv"))
     if (ncol(houses) == 27) {
       houses <- subset(houses, select = -Panelprogram)
     }
@@ -139,7 +192,7 @@ get_clean_all <- function(df) {
   df <- df %>% mutate(price_in_cur = ifelse(price_in_cur != "Ft", "EUR",
     ifelse(price_in_cur == "Ft", "HUF", "other")
   ))
-  options(digits=3)
+  options(digits = 3)
   df <- df %>% mutate(price = as.double(elementlist(str_split(price, " "), 1)))
   conversiourl <- "https://www.xe.com/currencyconverter/convert/?Amount=1&From=EUR&To=HUF"
   t <- read_html(conversiourl)
@@ -150,13 +203,11 @@ get_clean_all <- function(df) {
     elementlist(1) %>%
     substr(1, 3) %>%
     strtoi()
-  df <- df %>% mutate(price = ifelse(price_in_cur=="EUR",price*eur_to_huf/1000000,price))
-  
+  df <- df %>% mutate(price = ifelse(price_in_cur == "EUR", price * eur_to_huf / 1000000, price))
+
   return(df)
 }
-summary(df)
 
-setwd("~/GitHub/ECBS-5306-Coding-2-Web-Scraping-with-R/ingatlancom")
-# get_read_property()
-df <- get_all_property()
-get_clean_all(df)
+
+get_read_property("hungary","https://ingatlan.com/lista/elado+lakas")
+df <- get_allproperty("hungary") %>% get_clean_all()
